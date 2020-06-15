a592cdf85506182cd947f4dad5cb1e1a5bb3926e
alexcostabd <alexcosta1624@gmail.com>
2020-06-14 22:30:12 -0300

    #Criação da classe de conexão com Oracle 11g

---
+++ helpers/conexao.php
@@ -0,0 +1,42 @@
+<?php
+
+
+class ConexaoOracle
+{
+
+    public $host = "localhost"; 
+    public $usuario = "USER_MVC";   
+    public $senha = "alex";
+    public $porta = "1521";
+    public $conn = "";
+
+
+    public function __construct($host, $usuario, $senha, $porta, $conn)
+    {     
+
+       try 
+       {
+            $conn = new PDO("oci:dbname=//$host:$porta", $usuario, $senha);
+       } 
+        catch (PDOException $e) {
+            echo "Erro de Conexão " . $e->getMessage() . "<\br>";
+            exit;
+       }
+
+       unset($conn);
+    }
+
+    
+
+    
+
+    
+
+}
+
+
+
+
+
+?>
+
